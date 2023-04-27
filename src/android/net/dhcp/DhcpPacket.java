c7cab093d946e567a93d2c34a88c8153ac7d58f0
Pratyush <codelab@pratyush.dev>
2023-04-27 15:01:24 +0530

    hardcode android S as android version

--- src/android/net/dhcp/DhcpPacket.java
+++ src/android/net/dhcp/DhcpPacket.java
@@ -782,7 +782,7 @@ public abstract class DhcpPacket {
     private static String getVendorId(@Nullable List<DhcpOption> customizedClientOptions) {
         if (sTestOverrideVendorId != null) return sTestOverrideVendorId;
 
-        String vendorId = "android-dhcp-" + Build.VERSION.RELEASE;
+        String vendorId = "android-dhcp-" + Build.VERSION_CODES.S;
         if (customizedClientOptions != null) {
             for (DhcpOption option : customizedClientOptions) {
                 if (option.type == DHCP_VENDOR_CLASS_ID) {
